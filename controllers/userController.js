8da6b10ac8838580dd7da58b23d0097c4ba3ac09
syseoz <syseoz@naver.com>
2023-07-24 14:31:15 +0900

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,39 +1,32 @@
 import User from "../models/User.js";
 import Note from "../models/Note.js";
-import asyncHandler from 'express-async-handler'
-import bcrypt from 'bcrypt'
+import asyncHandler from "express-async-handler";
+import bcrypt from "bcrypt";
 
 /**
  * @dec get all users
  * @route /users
  */
-const getAllUsers = asyncHandler(async (req,res)=>{
-    console.log("hi")
-
-})
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find().select("-password");
+});
 
 /**
  * @dec post create users
  * @route /users
  */
-const createUser = asyncHandler(async (req,res)=>{
-    
-})
+const createUser = asyncHandler(async (req, res) => {});
 
 /**
  * @dec put update users
  * @route /users
  */
-const updateUser = asyncHandler(async (req,res)=>{
-    
-})
+const updateUser = asyncHandler(async (req, res) => {});
 
 /**
  * @dec delete delete users
  * @route /users
  */
-const deleteUser = asyncHandler(async (req,res)=>{
-    
-})
+const deleteUser = asyncHandler(async (req, res) => {});
 
-export {getAllUsers,updateUser,deleteUser,createUser};
+export { getAllUsers, updateUser, deleteUser, createUser };
